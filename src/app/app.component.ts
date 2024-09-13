0e6a000f38e3ca7e67b3a32b475bf7a66b20d4cd
Vijayaragavan <vijayaragavankts@gmail.com>
2024-09-13 12:27:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'navigationBar';
+  // 1st questionn
+  selectedMenu: string = '';
+
+
+  chooseMenu(menu: string) {
+    this.selectedMenu = menu;
+  }
+
+  // 2nd 
+  backgroundColor: string = 'white'; 
+
+  changeColor(color: string) {
+    this.backgroundColor = color;
+  }
+
+
+  // 3rd  
+  userInput: string = ''; 
+
+  displayInput() {
+    alert(this.userInput);
+  }
+  
 }
